cdafa2c1acce77994e01aa07343628d5ac7d8aa2
Jens Krumsieck <jenso666@live.de>
2022-02-04 11:08:35 +0100

    element class

---
+++ data/element.py
@@ -0,0 +1,36 @@
+import json
+import typing
+
+
+class Element:
+    """Small Element Type"""
+    def __init__(self, symbol: str, group: int, weight: float, number: int, name: str):
+        self.symbol = symbol
+        self.group = group
+        self.weight = weight
+        self.number = number
+        self.name = name
+
+    def __repr__(self) -> str:
+        return f"Element {self.symbol}"
+
+
+def get_element(symbol: str) -> Element:
+    """Returns Element with given Symbol"""
+    with open("elements.json", "r") as elements:
+        elements = json.load(elements)
+        element = list(filter(lambda el: symbol in el["Symbol"], elements))[0]
+        return Element(element["Symbol"], int(element["Group"]), float(element["AtomicWeight"]), int(element["AtomicNumber"]), element["Name"])
+
+
+def periodic_table() -> typing.Dict[str, Element]:
+    """returns complete periodic table as dictionary indexed by symbol"""
+    with open("elements.json", "r") as elements:
+        elements = json.load(elements)
+        list = {}
+        for element in elements:
+            if("Group" not in element):
+                element["Group"] = 0
+            list[element["Symbol"]] = Element(element["Symbol"], int(element["Group"]), float(
+                element["AtomicWeight"]), int(element["AtomicNumber"]), element["Name"])
+        return list
