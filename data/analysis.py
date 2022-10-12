bce17eea163e7d635430d7e52e67106ebdecb30e
Jens Krumsieck <jenso666@live.de>
2022-10-12 11:32:17 +0200

    create new merge script

---
+++ data/analysis.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Analysis:
+    Id: str
+    Type: str
+    DoopExp: float
+    DoopSim: float
+    Doming1: float
+    Saddling1: float
+    Ruffling1: float
+    WavingX1: float
+    WavingY1: float
+    Propellering: float
+    Doming2: float
+    Saddling2: float
+    Ruffling2: float
+    WavingX2: float
+    WavingY2: float
+    Propellering2: float
+    Cavity: float
